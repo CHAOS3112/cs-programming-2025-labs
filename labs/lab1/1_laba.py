288712b3a486c567d2efa054c4f6417e35d41e1c
CHAOS31 <142005384+CHAOS3112@users.noreply.github.com>
2025-09-28 22:59:26 +1000

    чтото сделал

---
+++ labs/lab1/1_laba.py
@@ -0,0 +1,61 @@
+# x1=1
+# x2=1.44
+# x3="ПИВО"
+# x4=True
+
+# q1=input("Имя епт")
+# q2=int(input("возраст епт"))
+# print(q1,q2)
+
+# w1=342
+# w2=56.2
+# w3="43"
+# w3=float(w3)
+# w2=w2
+# w1=w1
+# print(w1+w2+w3)
+
+# a=3
+# b=8
+# print((a+4*b)*(a-3*b)+a**2)
+
+# a=int(input("ээээ:"))
+# b=int(input("Хз лол:"))
+# print("считаем S прямоугольника", a*b, a*2+b*2)
+
+# print("*   *   *")
+# print(" * * * *")
+# print("  *   *")
+
+# q=int(input())
+# w=int(input())
+# print(q+w ,q-w ,q*w ,q/w ,q//w ,q%w ,q**w)
+# print(q==w)
+# print(q!=w)
+# print(q>w)
+# print(q<w)
+# print(q<=w)
+# print(q>=w)
+
+# q=input("ИМЯ ЕПТ:")
+# w=int(input("ВОЗРАСТ ЕПТ: "))
+# print(f"Меня зовут {q}, мне {w} лет.")
+
+# q="Съешь еще "
+# w="этих мягких "
+# e="французских булок, "
+# r="да выпей "
+# t='чаю'
+# print(q+w+e+r+t)
+
+# q="Нет! Да! "
+# print(q*4)
+
+# q=int(input("Чис r"))
+# w=int(input("Число w"))
+# e=int(input("Число q"))
+# a=(q+3)//2
+# print(f'Результат вычисления: {a}')
+
+q="ШКИБИДИДОПДОПЕСЕС"
+print(q[0:4], q[-2:], q[3:8], q[::-1])
