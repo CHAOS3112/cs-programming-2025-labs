eb8b08d72ad54187150bfe8d4dfcb260b2bedbae
CHAOS31 <142005384+CHAOS3112@users.noreply.github.com>
2025-12-05 11:53:49 +1000

    гоаыопо

--- labs/lab4/4.5.py
+++ labs/lab4/4.5.py
@@ -1,9 +1,10 @@
 pas= input("введите пароль: ")
 def chek(password):
-    if len(password)<8: return print("пароль должен содержать не менее 8 символов")
-    if password.lower()== password: return print("пароль должен содержать хотябы 1 заглавный символ")
-    if password.upper()== password: return print("пароль должен содержать хотябы 1 символ нижнего регистра")
-    if not any(x.isdigit() for x in password): return print("пароль должен содержать хотябы 1 цифру")
-    if not any(x.isalnum() for x in password): return print("пароль должен содержать хотябы 1 специальный символ")
-    else: return print("пароль установлен")
-print(chek(pas))
+    if len(password)<8: print("пароль должен содержать не менее 8 символов")
+    if password.lower()== password: print("пароль должен содержать хотябы 1 заглавный символ")
+    if password.upper()== password:  print("пароль должен содержать хотябы 1 символ нижнего регистра")
+    if not any(x.isdigit() for x in password):  print("пароль должен содержать хотябы 1 цифру")
+    if not any(x.isalnum() for x in password):  print("пароль должен содержать хотябы 1 специальный символ")
+    else: print("пароль установлен")
+
+chek(pas)
